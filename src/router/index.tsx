53ddc7d23e5162f14dd3d31fb938878c4e39edcf
YogaDharma21 <yogacode2006@gmail.com>
2026-01-30 19:16:19 +0800

    feat(ui): slice signin page from HTML

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import ManagerHome from "../pages/manager/home";
+import SignInPage from "./SignIn";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <ManagerHome />
+    },
+    {
+        path: "/manager/sign-in",
+        element: <SignInPage />
     }
 ])
  export default router;
