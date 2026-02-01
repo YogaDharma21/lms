80c78639b8420fe045c4102fd8dd9ea8c536188c
YogaDharma21 <yogacode2006@gmail.com>
2026-02-01 08:51:15 +0800

    feat(ui): student detail course

--- src/pages/student/StudentOverview/index.tsx
+++ src/pages/student/StudentOverview/index.tsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router";
+import CardCourse from "./CardCourse";
+
 
 export default function StudentPage() {
     return (
@@ -9,126 +10,12 @@ export default function StudentPage() {
             <h2 className="font-extrabold text-[22px] leading-[33px]">
                 Latest Courses
             </h2>
-            <div className="card flex items-center gap-5">
-                <div className="flex shrink-0 w-[100px] h-20 rounded-[20px] bg-[#D9D9D9] overflow-hidden">
-                    <img
-                        alt="thumbnail"
-                        className="w-full h-full object-cover"
-                        src="/assets/images/thumbnails/th-1.png"
-                    />
-                </div>
-                <div className="w-full">
-                    <Link
-                        className="font-bold text-xl leading-[30px] line-clamp-1"
-                        to="#"
-                    >
-                        Responsive Design Triclorem Lorem, ipsum dolor.
-                    </Link>
-                    <div className="flex items-center gap-[6px] mt-[6px]">
-                        <img
-                            alt="icon"
-                            src="/assets/images/icons/crown-purple.svg"
-                        />
-                        <p className="text-[#838C9D]">Programming</p>
-                    </div>
-                </div>
-            </div>
-            <div className="card flex items-center gap-5">
-                <div className="flex shrink-0 w-[100px] h-20 rounded-[20px] bg-[#D9D9D9] overflow-hidden">
-                    <img
-                        alt="thumbnail"
-                        className="w-full h-full object-cover"
-                        src="/assets/images/thumbnails/th-2.png"
-                    />
-                </div>
-                <div className="w-full">
-                    <Link
-                        className="font-bold text-xl leading-[30px] line-clamp-1"
-                        to="#"
-                    >
-                        HTMX JavaScript 2020
-                    </Link>
-                    <div className="flex items-center gap-[6px] mt-[6px]">
-                        <img
-                            alt="icon"
-                            src="/assets/images/icons/crown-purple.svg"
-                        />
-                        <p className="text-[#838C9D]">Marketing</p>
-                    </div>
-                </div>
-            </div>
-            <div className="card flex items-center gap-5">
-                <div className="flex shrink-0 w-[100px] h-20 rounded-[20px] bg-[#D9D9D9] overflow-hidden">
-                    <img
-                        alt="thumbnail"
-                        className="w-full h-full object-cover"
-                        src="/assets/images/thumbnails/th-3.png"
-                    />
-                </div>
-                <div className="w-full">
-                    <Link
-                        className="font-bold text-xl leading-[30px] line-clamp-1"
-                        to="#"
-                    >
-                        Mastering React
-                    </Link>
-                    <div className="flex items-center gap-[6px] mt-[6px]">
-                        <img
-                            alt="icon"
-                            src="/assets/images/icons/crown-purple.svg"
-                        />
-                        <p className="text-[#838C9D]">Data Science</p>
-                    </div>
-                </div>
-            </div>
-            <div className="card flex items-center gap-5">
-                <div className="flex shrink-0 w-[100px] h-20 rounded-[20px] bg-[#D9D9D9] overflow-hidden">
-                    <img
-                        alt="thumbnail"
-                        className="w-full h-full object-cover"
-                        src="/assets/images/thumbnails/th-2.png"
-                    />
-                </div>
-                <div className="w-full">
-                    <Link
-                        className="font-bold text-xl leading-[30px] line-clamp-1"
-                        to="#"
-                    >
-                        Responsive Design Triclorem Lorem, ipsum dolor.
-                    </Link>
-                    <div className="flex items-center gap-[6px] mt-[6px]">
-                        <img
-                            alt="icon"
-                            src="/assets/images/icons/crown-purple.svg"
-                        />
-                        <p className="text-[#838C9D]">Marketing</p>
-                    </div>
-                </div>
-            </div>
-            <div className="card flex items-center gap-5">
-                <div className="flex shrink-0 w-[100px] h-20 rounded-[20px] bg-[#D9D9D9] overflow-hidden">
-                    <img
-                        alt="thumbnail"
-                        className="w-full h-full object-cover"
-                        src="/assets/images/thumbnails/th-3.png"
-                    />
-                </div>
-                <div className="w-full">
-                    <Link
-                        className="font-bold text-xl leading-[30px] line-clamp-1"
-                        to="#"
-                    >
-                        Company Profile Multilorem Lorem, ipsum dolor.
-                    </Link>
-                    <div className="flex items-center gap-[6px] mt-[6px]">
-                        <img
-                            alt="icon"
-                            src="/assets/images/icons/crown-purple.svg"
-                        />
-                        <p className="text-[#838C9D]">Data Science</p>
-                    </div>
-                </div>
-            </div>
+            <CardCourse
+                id={1}
+                imageUrl="/assets/images/thumbnails/th-1.png"
+                title="Responsive Design Triclorem Lorem, ipsum dolor."
+                category="Programming"
+            />
         </section>
     );
 }
