32e1dc22d29df77c4a91bba83948186ce1338cb0
YogaDharma21 <yogacode2006@gmail.com>
2026-02-07 13:03:52 +0800

    feat: implement course creation with thumbnail upload and course listing endpoints.

--- apps/backend/courseRoute.js
+++ apps/backend/courseRoute.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { getCourses } from "../controllers/courseController.js";
+import multer from "multer";
+import { getCourses, postCourse } from "../controllers/courseController.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
+import { fileFilter, fileStorageCourse } from "../utils/multer.js";
 
 const courseRoutes = express.Router();
 
+const upload = multer({
+    storage: fileStorageCourse,
+    fileFilter: fileFilter,
+});
+
 courseRoutes.get("/courses", verifyToken, getCourses);
+courseRoutes.post(
+    "/courses",
+    verifyToken,
+    upload.single("thumbnail"),
+    postCourse,
+);
 
 export default courseRoutes;
