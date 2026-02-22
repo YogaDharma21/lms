4b364be10bd87df9abfc8bfa21e0c0a8840f996f
YogaDharma21 <yogacode2006@gmail.com>
2026-02-22 13:20:47 +0800

    feat(api): add delete student endpoint

--- apps/backend/studentRoutes.js
+++ apps/backend/studentRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
-import { getStudents, postStudent, updateStudent } from "../controllers/studentController.js";
+import {
+    deleteStudent,
+    getStudents,
+    postStudent,
+    updateStudent,
+} from "../controllers/studentController.js";
 import multer from "multer";
 import { fileStorage, fileFilter } from "../utils/multer.js";
 const studentRoutes = express.Router();
@@ -11,17 +16,21 @@ const upload = multer({
 });
 
 studentRoutes.get("/students", verifyToken, getStudents);
+
 studentRoutes.post(
     "/students",
     verifyToken,
     upload.single("avatar"),
     postStudent,
 );
+
 studentRoutes.put(
     "/students/:id",
     verifyToken,
     upload.single("avatar"),
     updateStudent,
 );
+
+studentRoutes.delete("/students/:id", verifyToken, deleteStudent);
 
 export default studentRoutes;
